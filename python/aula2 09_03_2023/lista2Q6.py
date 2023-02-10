b813511e706247ff6979baa0845e30dcf9863092
Ricardo1592 <rcmm@cin.ufpe.br>
2023-02-09 22:06:52 -0300

    Subindo o build_task para vscode

--- python/aula2 09_03_2023/lista2Q6.py
+++ python/aula2 09_03_2023/lista2Q6.py
@@ -1,2 +1,6 @@
 # Lê uma lista de tamanho n e imprime duas, uma impar, outra par mais ela mesmo
 n=input("Digite o tamanho da lista:")
+v1=[0]*n
+for i in range(n):
+    v1[i]=input("Digite um número:")
+    
