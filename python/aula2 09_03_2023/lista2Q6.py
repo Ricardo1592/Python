353c1f0439b6797ffbcc6c9ebe08f051115b8eb1
Ricardo1592 <rcmm@cin.ufpe.br>
2023-02-09 23:36:17 -0300

    Adicionando questões lista2

--- python/aula2 09_03_2023/lista2Q6.py
+++ python/aula2 09_03_2023/lista2Q6.py
@@ -1,6 +1,19 @@
 # Lê uma lista de tamanho n e imprime duas, uma impar, outra par mais ela mesmo
-n=input("Digite o tamanho da lista:")
+n=int(input("Digite o tamanho da lista:"))
 v1=[0]*n
+v_impar=[]
+v_par=[]
 for i in range(n):
-    v1[i]=input("Digite um número:")
+    v1[i]=int(input("Digite um número:"))
     
+for i in range(n):
+    if v1[i]%2 ==0:
+        v_par=v_par+[v1[i]]
+    else:    
+        v_impar=v_impar+[v1[i]]
+
+
+print("A lista digitada é --> %s " %v1)
+print("A lista número impar --> %s " %v_impar)
+print("A lista número par --> %s " %v_par)
+
