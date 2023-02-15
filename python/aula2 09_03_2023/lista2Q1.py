c63a828805e9862024eb20c74f7e94b59d91e78c
Ricardo César <rcmm@cin.ufpe.br>
2023-02-15 12:06:47 -0300

    subindo aula6 15_02_2023

--- python/aula2 09_03_2023/lista2Q1.py
+++
@@ -1,5 +0,0 @@
-# Ler o nome do usuário e imprime na vertical
-
-nome=input("Digite o seu nome:")
-for letra in nome:
-    print(letra)
--- python/aula3 10_02_2023/teste.py
+++
@@ -1,5 +0,0 @@
-a=14
-if a < 13:
-    print("maior que 13")
-elif a < 15:
-    print("menor que 15")    
