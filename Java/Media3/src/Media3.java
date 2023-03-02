1e2acc6b3ff532fcfba7660cf8a97f6fc2b53a3b
Ricardo César <rcmm@cin.ufpe.br>
2023-03-02 16:53:35 -0300

    Aula03 Java

---
+++ Java/Media3/src/Media3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Media3 {
+	public static void main(String [] args){
+		
+		System.out.print("Digite 3 números");
+		Scanner input= new Scanner(System.in);
+		int n1;
+		int n2;
+		int n3;
+		n1=input.nextInt();
+		n2=input.nextInt();
+		n3=input.nextInt();
+		
+		
+	}
+
+}
